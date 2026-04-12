d59f6b4c7b9600e61d03c3450f7b2bbb6e700102
tyleo <git@tyleo.com>
2026-04-12 10:12:27 -0700

    partial: faces-to-net initial implementation

--- projects/tyt-cubemap/src/commands/mod.rs
+++ projects/tyt-cubemap/src/commands/mod.rs
@@ -1,9 +1,11 @@
 mod equirect_to_faces;
 mod equirect_to_net;
 mod faces_to_equirect;
+mod faces_to_net;
 mod pixelate_faces;
 
 pub use equirect_to_faces::*;
 pub use equirect_to_net::*;
 pub use faces_to_equirect::*;
+pub use faces_to_net::*;
 pub use pixelate_faces::*;
--- projects/tyt-cubemap/src/tyt_cubemap.rs
+++ projects/tyt-cubemap/src/tyt_cubemap.rs
@@ -5,14 +5,17 @@ use clap::Subcommand;
 #[derive(Clone, Debug, Subcommand)]
 #[command(subcommand_value_name = "command")]
 pub enum TytCubemap {
-    #[command(name = "faces-to-equirect")]
-    FacesToEquirect(commands::FacesToEquirect),
+    #[command(name = "equirect-to-faces")]
+    EquirectToFaces(commands::EquirectToFaces),
 
     #[command(name = "equirect-to-net")]
     EquirectToNet(commands::EquirectToNet),
 
-    #[command(name = "equirect-to-faces")]
-    EquirectToFaces(commands::EquirectToFaces),
+    #[command(name = "faces-to-equirect")]
+    FacesToEquirect(commands::FacesToEquirect),
+
+    #[command(name = "faces-to-net")]
+    FacesToNet(commands::FacesToNet),
 
     #[command(name = "pixelate-faces")]
     PixelateFaces(commands::PixelateFaces),
@@ -21,9 +24,10 @@ pub enum TytCubemap {
 impl TytCubemap {
     pub fn execute(self, dependencies: impl crate::Dependencies) -> crate::Result<()> {
         match self {
-            TytCubemap::FacesToEquirect(cmd) => cmd.execute(dependencies),
-            TytCubemap::EquirectToNet(cmd) => cmd.execute(dependencies),
             TytCubemap::EquirectToFaces(cmd) => cmd.execute(dependencies),
+            TytCubemap::EquirectToNet(cmd) => cmd.execute(dependencies),
+            TytCubemap::FacesToEquirect(cmd) => cmd.execute(dependencies),
+            TytCubemap::FacesToNet(cmd) => cmd.execute(dependencies),
             TytCubemap::PixelateFaces(cmd) => cmd.execute(dependencies),
         }
     }
